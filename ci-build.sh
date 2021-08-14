c2a0de365db18c6132f4a74e79ef3985cd2f54b3
Tarik02 <Taras.Fomin@gmail.com>
2021-08-14 15:57:26 +0300

    Separate binaries for macOS x86_64 and arm64
    Signed-off-by: Tarik02 <Taras.Fomin@gmail.com>

--- ci-build.sh
+++ ci-build.sh
@@ -2,4 +2,11 @@
 
 scriptPath="`dirname \"$0\"`"
 
-$scriptPath/build-native.sh release
+if [ ! -z "$OSX_TARGET_ARCHITECTURE" ]; then
+  $scriptPath/build-native.sh release -osx-arch $OSX_TARGET_ARCHITECTURE
+  if [[ "$OSX_TARGET_NAME" != "cimgui.dylib" ]]; then
+    mv cimgui/build/Release/cimgui.dylib cimgui/build/Release/$OSX_TARGET_NAME
+  fi
+else
+  $scriptPath/build-native.sh release
+fi
